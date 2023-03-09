bc5a896d0e2e4eef4f2944f1fab3de11219dd99d
karanmaurya15 <karanmaurya9663@gmail.com>
2023-03-09 21:25:00 +0530

    [Karan] added Reset Pasword Components

--- Fundoo/src/app/app.module.ts
+++ Fundoo/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegistrationComponent } from './Components/registration/registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
 
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
